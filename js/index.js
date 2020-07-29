59fd6894cf8d02acc8a8949cb4c5afda2ec7add0
saira-ashlee <saira-ashlee@users.noreply.github.com>
2020-07-29 14:10:07 -0600

    Add Existing Files
    Adding Exisiting files from last week's homework to continue on.

---
+++ js/index.js
@@ -0,0 +1,41 @@
+/*console.log("your index.js file is loaded correctly!")*/
+
+
+/*Smooth Scrolling*/
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".workNav").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+/*Active Navigation Tab*/
+  var header = document.getElementById("navLinks");
+  var btns = header.getElementsByClassName("navItems");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+    });
+  }
